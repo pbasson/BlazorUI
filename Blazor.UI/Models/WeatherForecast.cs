70b5ea4d2e4d57e8ac65c91590ad7bb6f50a8112
Preet Basson <preet.basson@gmail.com>
2024-10-03 21:45:31 +0100

    Reapply "0.03: Update"
    This reverts commit 5fcda2156cc04b62ecc72046721bf8d468570df2.

--- Blazor.UI/Models/WeatherForecast.cs
+++ Blazor.UI/Models/WeatherForecast.cs
@@ -2,7 +2,8 @@ namespace Blazor.UI.Models
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
+        public string? DateDisplay => Date.ToString("dd-MM-yyyy");    
         public int TemperatureC { get; set; }
         public string? Summary { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
