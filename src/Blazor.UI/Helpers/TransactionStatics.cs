b005a97464c2d8280e21e853cdef5d08a36bde1d
preetpal basson <preet.basson@gmail.com>
2025-01-16 22:41:36 +0000

    0.25: Update

--- src/Blazor.UI/Helpers/TransactionStatics.cs
+++ src/Blazor.UI/Helpers/TransactionStatics.cs
@@ -8,6 +8,10 @@ public readonly struct TransactionStatics
 
 public readonly struct TransactionNavigation
 {
-    public readonly static string GetAllRecords = "CRUDTransaction/GetAllRecords";
-    public readonly static string GetRecordById = "CRUDTransaction/GetRecordById/{Id:int}";
+    public readonly static string Module = "CRUDTransaction";
+    public readonly static string GetAllRecords = $"{Module}/GetAllRecords";
+    public readonly static string GetRecordById = $"{Module}/GetRecordById/{0}";
+    public readonly static string CreateRecord = $"{Module}/CreateRecord/";
+    public readonly static string UpdateRecord = $"{Module}/UpdateRecord/";
+    public readonly static string DeleteRecord = $"{Module}/DeleteRecord/{0}";
 }
